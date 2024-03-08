0590f08391791f752ecc92d68981014d05a6d75f
noah Kim <cpn1011@gmail.com>
2024-03-08 20:53:21 +0900

    MovieDetail Page #1

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,24 @@ import LandingPage from "./components/views/LandingPage/LandingPage.js";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 import Auth from "./hoc/auth.js";
+import MovieDetail from "./components/views/MovieDetail/MovieDetail.js";
+import NavBar from "./components/views/NavBar/NavBar.js";
 
 function App() {
   // Auth(LoginPage,false) 의미 : 로그인 페이지는 로그인하지 않은 유저만 접근 가능
   return (
     <div>
       <Router>
+        <NavBar></NavBar>
         <Routes>
           <Route exact path="/" Component={Auth(LandingPage, null)} />
           <Route exact path="/login" Component={Auth(LoginPage, false)} />
           <Route exact path="/register" Component={Auth(RegisterPage, false)} />
+          <Route
+            exact
+            path="/movie/:movieId"
+            Component={Auth(MovieDetail, null)}
+          />
           {/* <Route exact path="/" Component={LandingPage} />
           <Route exact path="/login" Component={LoginPage} />
           <Route exact path="/register" Component={RegisterPage} /> */}
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,26 +3,34 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MainImage from "./Sections/MainImage";
+import GridCards from "../commons/GridCard";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../config";
+import { Row } from "antd";
 
 function LandingPage() {
   const navigate = useNavigate();
   const [Movies, setMovies] = useState([]);
   const [MainMovieImage, setMainMovieImage] = useState(null);
+  const [CurrentPage, setCurrentPage] = useState(0);
 
   // LandingPage가 랜딩되자마자 실행되는 코드
   useEffect(() => {
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetchMovies(endpoint);
+  }, []);
+
+  const fetchMovies = (endpoint) => {
     axios
       .get(endpoint) //
       .then((res) => {
-        console.log(res.data.results);
-        setMovies([res.data.results]);
+        console.log(res.data.results, res);
+        setMovies([...Movies, ...res.data.results]);
         setMainMovieImage(res.data.results[0]);
+        setCurrentPage(res.data.page + 1);
       })
       .catch((err) => console.log("에러", err));
-  }, []);
-
+  };
+  // console.log(Movies);
   const onClickHandler = () => {
     axios.get("/api/users/logout").then((res) => {
       // console.log(res.data);
@@ -32,6 +40,12 @@ function LandingPage() {
         alert("failed to logout");
       }
     });
+  };
+
+  // loadmore items 버튼
+  const loadMoreItems = () => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage}`;
+    fetchMovies(endpoint);
   };
 
   return (
@@ -49,9 +63,26 @@ function LandingPage() {
           <h2>Movies by latest</h2>
           <hr />
           {/* Movie Grid Cards */}
+          <Row gutter={[16, 16]}>
+            {Movies &&
+              Movies.map((movie, index) => (
+                <React.Fragment key={index}>
+                  <GridCards
+                    landingPage
+                    image={
+                      movie.poster_path
+                        ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                        : null
+                    }
+                    movieId={movie.id}
+                    movieName={movie.original_title}
+                  ></GridCards>
+                </React.Fragment>
+              ))}
+          </Row>
         </div>
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <button>Load More</button>
+          <button onClick={loadMoreItems}>Load More</button>
         </div>
       </div>
       <div
